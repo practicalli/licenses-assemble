dc96beae154b770c716a5320c6fc65e5899ee909
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2022-02-24 15:30:06 +0000

    new project: using deps-new app template
    Install deps-new as a tool
    clojure -Ttools install io.github.seancorfield/deps-new '{:git/tag "v0.4.9"}' :as project-new
    
    Create new project from app template
    clojure -Tproject-new app :name practicalli/licenses-assemble
    
    Removed license file

---
+++ test/practicalli/licenses_assemble_test.clj
@@ -0,0 +1,7 @@
+(ns practicalli.licenses-assemble-test
+  (:require [clojure.test :refer :all]
+            [practicalli.licenses-assemble :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
